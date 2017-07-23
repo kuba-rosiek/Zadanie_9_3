073bf9c167ef8bfcd5ebc485fe5081c4b5ae3b67
unknown <rosiek.kuba@gmail.com>
2017-07-23 15:04:40 +0100

    Wyswietla polowe

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var dinosaur = 'triceratops';
+var dinosaurUpperCased = dinosaur.toUpperCase();
+var textCharsAfter = text.replace('Velociraptor', 'Dinosaur');
+var partText = textCharsAfter.slice(length/2);
+console.log(partText);
